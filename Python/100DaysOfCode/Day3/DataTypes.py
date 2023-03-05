02c7a636807074ebd46372f347987fb2370f6c60
Chaitaly Kundu <chaitaly.slg01@gmail.com>
2023-03-05 22:12:38 +0530

    Add files via upload

---
+++ Python/100DaysOfCode/Day3/DataTypes.py
@@ -0,0 +1,17 @@
+a = 1
+b = "Chaitaly"
+c = True
+d = None
+e = 1.1
+f = complex(8, 1)
+g = [1, 2, 3, [4, 5], ["apple", "banana"]]
+h = ("apple", "banana", ("orange", "berry"))
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))
+print(type(h))
