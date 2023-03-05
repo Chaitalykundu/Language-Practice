36bf40b0474ad02c177d0e93de82a997f6acee9e
Chaitaly Kundu <chaitaly.slg01@gmail.com>
2023-03-05 22:11:27 +0530

    Add files via upload

---
+++ Python/100DaysOfCode/Day2/Comment.py
@@ -0,0 +1,8 @@
+# A comment is a part of coding that programmer doesnot want to execute rather they use this to explain the code
+
+"""
+Author : Chaitaly Kundu
+Date : 28/02/2023
+"""
+
+print("Upper things are commented")
